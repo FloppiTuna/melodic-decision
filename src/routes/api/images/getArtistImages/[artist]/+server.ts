41568a6cc28f58f188d5e84b49df1e03af117e47
Griffin <griffin@edforma.app>
2025-12-15 07:58:42 -0600

    error handling

--- src/routes/api/images/getArtistImages/[artist]/+server.ts
+++ src/routes/api/images/getArtistImages/[artist]/+server.ts
@@ -1,13 +1,21 @@
 export async function GET({ params }): Promise<void | Response> {
     const artist = params.artist;
 
-    console.log(`Fetching images for artist: ${artist}`);
-
     const options = {method: 'GET', headers: {'User-Agent': 'insomnia/12.1.0'}};
 
     const resp = fetch(`https://webservice.fanart.tv/v3.2/music/${artist}?api_key=e3d97fe7054cbdebd163840a53a883dd`, options)
         .then(response => response.json())
         .then((response) => {
+            if (response.error) {
+                console.warn(`error fetching artist photos for "${artist}"!`)
+                return new Response(JSON.stringify(response), {
+                    status: response.error == "Artist not found" ? 404 : 500, //todo: this sucks
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+            }
+
             return new Response(JSON.stringify(response), {
                 status: 200,
                 headers: {
